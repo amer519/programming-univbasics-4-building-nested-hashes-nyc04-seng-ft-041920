3f234b3e1774f3226a98f0c4dc069aefd7384884
Amer Fahmy <afahmy519@gmail.com>
2020-04-29 16:50:16 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -1,5 +1,5 @@
 def bash_hash_method
-  bash_hash = {
+  base_hash = {
     :railroads => {}
   }
 end
@@ -10,5 +10,39 @@ end
 
 
 
+
+
+monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 
 
