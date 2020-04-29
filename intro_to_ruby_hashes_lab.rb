0c731f91b3cc1929372867342be820ed746069c2
Amer Fahmy <afahmy519@gmail.com>
2020-04-29 18:18:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,10 @@ end
 def monopoly_with_second_tier
   base_hash = {
     :railroads => {
-      pieces: "4"
-      rent_in_dollars: {}
+      pieces: "4",
+      rent_in_dollars: {},
       names: {}
-    
+    }
 }
   
 end
